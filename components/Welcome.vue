bed1966d8447e9c0d0f735033592a5f0bb30088f
Human Town <ty.1997@foxmail.com>
2022-04-27 11:19:42 +0800

    配置文件

---
+++ components/Welcome.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    asd
+    阿斯顿
+    Google123123123阿斯顿请问去万达是的
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <span>{{ title }}</span>
+    </div>
+</template>
+
+<script lang='ts' setup>
+const title = ref<string>('new page')
+</script>
+
+<style scoped>
+</style>
